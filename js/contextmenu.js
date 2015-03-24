23f06fb5a23bf33d4e3ee88f2a25175ced3d74f7
Katherine Landreth <katlandreth@gmail.com>
2015-03-24 16:14:41 -0400

    make a seperate js file for the context menu HTML

---
+++ js/contextmenu.js
@@ -0,0 +1,12 @@
+var contextMenu = '<nav class="circular-menu">'+
+                    '<div class="circle">'+
+                    '<ul class="circle-menu">'+
+                    '<li><span class="fa-refresh fa"></span><button class="spnlt fa-angle-left fa"></button><button class="spnrt fa-angle-                      right fa"></button></li>'+
+                    '<li class="widelist"><span class="fa-cube fa" id="smallcube"></span><span class="fa fa-cube" id="bigcube"></span>                          <input id="slider" class="scale" type=range id=scale min=1 value=100 max=200 step=1 "></li>'+
+                    '<li class="fa-tint fa"><input id="colorpicker" type="color"></li>'+
+                    '<li><span class="cow"></span><input type="radio" name="motion" value="on"><input type="radio" name="motion"                                value="off" checked="checked"></li>'+
+                    '<li><span class="fa-reply fa pointup"></span><button class="spnup fa-angle-up fa"></button><button                                        class="spndwn fa-angle-down fa"></li>'+
+                    '</div>'+
+                    '<li class="menu-button fa-arrows-alt fa"></li>'+
+                    '</ul>'+
+                    '</nav>';
