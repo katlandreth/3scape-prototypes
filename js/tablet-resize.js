862ac45a460752878e1fef5651a4454c8fb9080f
Katherine Landreth <katlandreth@gmail.com>
2015-04-01 16:23:56 -0400

    makes icons responsive and centered

--- js/tablet-resize.js
+++ js/tablet-resize.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     var ew = $('.innerdial, .meter-outer, #meter-inner').css("width");
-    console.log(ew);
+    var fontSize = parseInt(ew)/2;
     $('.innerdial, .meter-outer, #meter-inner').css({'height':ew});
+    $('span.fa').css({'line-height':ew, 'font-size': fontSize});
     $( window ).bind("resize", function(){
         var ew = $('.innerdial, .meter-outer, #meter-inner').css("width");
-        console.log(ew);
+        var fontSize = ew ;
+        var fontSize = parseInt(ew)/2;
         $('.innerdial, .meter-outer, #meter-inner').css({'height':ew});
+        $('span.fa').css({'line-height':ew, 'font-size': fontSize});
     });
 });
