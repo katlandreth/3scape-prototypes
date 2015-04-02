8b6a6ab96eb3f6cb5396ddf4f7f6213a12bf3696
Katherine Landreth <katlandreth@gmail.com>
2015-04-02 11:59:31 -0400

    adds ring fixes fluid sizing

--- js/tablet-resize.js
+++ js/tablet-resize.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
     var ew = $('.innerdial, .meter-outer, #meter-inner').css("width");
-    var fontSize = parseInt(ew)/2;
-    $('.innerdial, .meter-outer, #meter-inner').css({'height':ew});
-    $('span.fa').css({'line-height':ew, 'font-size': fontSize});
+    var fontSize = parseInt(ew)/2; 
+    var partWidth = $('li.part').css('width');
+    var buttonWidth = $('button.parts-btn').css('width');
+    $('.innerdial, .meter-outer, #meter-inner').css({'height':ew}); //makes height = fluid width
+    $('span.fa').css({'line-height':ew, 'font-size': fontSize}); //makes icon in dial responsive to dial size change
+    $('li.part').css({'line-height':partWidth});
+    $('.logo, .nav-menu').css({'height': partWidth});
+    $('button.parts-btn').css({'height': buttonWidth});
+    
     $( window ).bind("resize", function(){
         var ew = $('.innerdial, .meter-outer, #meter-inner').css("width");
         var fontSize = ew ;
         var fontSize = parseInt(ew)/2;
+        var partWidth = $('li.part').css('width');
+         var buttonWidth = $('button.parts-btn').css('width');
         $('.innerdial, .meter-outer, #meter-inner').css({'height':ew});
         $('span.fa').css({'line-height':ew, 'font-size': fontSize});
+        $('li.part').css({'line-height':partWidth});
+        $('.logo, .nav-menu').css({'height': partWidth});
+        $('button.parts-btn').css({'height': buttonWidth});
     });
 });
