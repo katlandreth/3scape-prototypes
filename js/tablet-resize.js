db8287a9867fc27ff9e1047035a74dc715bfaa68
Katherine Landreth <katlandreth@gmail.com>
2015-04-02 12:52:40 -0400

    cleans up resize js file

--- js/tablet-resize.js
+++ js/tablet-resize.js
@@ -1,24 +1,18 @@
-$(document).ready(function(){
-    var ew = $('.innerdial, .meter-outer, #meter-inner').css("width");
-    var fontSize = parseInt(ew)/2; 
-    var partWidth = $('li.part').css('width');
-    var buttonWidth = $('button.parts-btn').css('width');
-    $('.innerdial, .meter-outer, #meter-inner').css({'height':ew}); //makes height = fluid width
-    $('span.fa').css({'line-height':ew, 'font-size': fontSize}); //makes icon in dial responsive to dial size change
-    $('li.part').css({'line-height':partWidth});
-    $('.logo, .nav-menu').css({'height': partWidth});
-    $('button.parts-btn').css({'height': buttonWidth});
-    
-    $( window ).bind("resize", function(){
+function setHeights(){
         var ew = $('.innerdial, .meter-outer, #meter-inner').css("width");
-        var fontSize = ew ;
-        var fontSize = parseInt(ew)/2;
+        var fontSize = parseInt(ew)/2; 
         var partWidth = $('li.part').css('width');
-         var buttonWidth = $('button.parts-btn').css('width');
-        $('.innerdial, .meter-outer, #meter-inner').css({'height':ew});
-        $('span.fa').css({'line-height':ew, 'font-size': fontSize});
+        var buttonWidth = $('button.parts-btn').css('width');
+        $('.innerdial, .meter-outer, #meter-inner').css({'height':ew}); //makes height = fluid width
+        $('span.fa').css({'line-height':ew, 'font-size': fontSize}); //makes icon in dial responsive to dial size change
         $('li.part').css({'line-height':partWidth});
         $('.logo, .nav-menu').css({'height': partWidth});
         $('button.parts-btn').css({'height': buttonWidth});
+    }
+
+$(document).ready(function(){
+    setHeights();
+    $(window).resize(function () {
+        setHeights();
     });
 });
